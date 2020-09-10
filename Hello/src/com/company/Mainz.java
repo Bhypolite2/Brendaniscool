f7e4b54b3c7ca11775b95180fb975716bbaa42df
bhypolite2 <bhypolite63@gmail.com>
2020-09-10 13:47:53 -0400

    This Is Homwork

--- Hello/src/com/company/Mainz.java
+++ Hello/src/com/company/Mainz.java
@@ -1,9 +1,59 @@
 package com.company;
 
+
 public class Mainz {
 
-    public static void main(String[] args) {
-        System.out.println("BrendanisCool");
-        
-    }
-}
+
+
+
+    public static void main (String[]args) {
+
+
+        }
+
+
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
